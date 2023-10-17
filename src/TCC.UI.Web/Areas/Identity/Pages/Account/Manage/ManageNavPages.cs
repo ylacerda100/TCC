f5a130dd26447512b5fcdca41284f5497f7d0a33
Ygor Lacerda <ygor.l@financas360.com.br>
2023-10-17 19:31:28 -0300

    adicionada pagina de pedidos

--- src/TCC.UI.Web/Areas/Identity/Pages/Account/Manage/ManageNavPages.cs
+++ src/TCC.UI.Web/Areas/Identity/Pages/Account/Manage/ManageNavPages.cs
@@ -61,6 +61,8 @@ namespace  TCC.UI.Web.Areas.Identity.Pages.Account.Manage
         /// </summary>
         public static string TwoFactorAuthentication => "TwoFactorAuthentication";
 
+        public static string Pedidos => "Pedidos";
+
         /// <summary>
         ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
         ///     directly from your code. This API may change or be removed in future releases.
@@ -108,6 +110,8 @@ namespace  TCC.UI.Web.Areas.Identity.Pages.Account.Manage
         ///     directly from your code. This API may change or be removed in future releases.
         /// </summary>
         public static string TwoFactorAuthenticationNavClass(ViewContext viewContext) => PageNavClass(viewContext, TwoFactorAuthentication);
+
+        public static string PedidosNavClass(ViewContext viewContext) => PageNavClass(viewContext, Pedidos);
 
         /// <summary>
         ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
