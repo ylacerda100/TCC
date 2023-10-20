7b5ef7e2cea9939cbf393ffd4ccf272eb72af238
Ygor Lacerda <ygor.l@financas360.com.br>
2023-10-20 12:11:06 -0300

    pdf to img

--- src/TCC.UI.Web/Controllers/AulasController.cs
+++ src/TCC.UI.Web/Controllers/AulasController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using PdfiumViewer;
+using System.Drawing;
+using System.Drawing.Imaging;
 using TCC.Application.Interfaces;
 
 namespace TCC.UI.Web.Controllers
@@ -7,10 +10,12 @@ namespace TCC.UI.Web.Controllers
     public class AulasController : Controller
     {
         private readonly IAulaAppService _aulaAppService;
+        private readonly IWebHostEnvironment _env;
 
-        public AulasController(IAulaAppService aulaAppService)
+        public AulasController(IAulaAppService aulaAppService, IWebHostEnvironment env)
         {
             _aulaAppService = aulaAppService;
+            _env = env;
         }
 
         public IActionResult Index()
@@ -32,6 +37,22 @@ namespace TCC.UI.Web.Controllers
             if (aulaViewModel is null)
             {
                 return NotFound();
+            }
+
+            var filePath = Path.Combine(
+                _env.ContentRootPath, "wwwroot", "assets", "pdf", aulaViewModel.ContentUrl);
+
+            using (var pdfDocument = PdfDocument.Load(filePath))
+            {
+                var images = new List<byte[]>();
+
+                var bitmapImage = pdfDocument.Render(0, 700, 700, true);
+
+                ImageConverter converter = new ImageConverter();
+                var imgBytes = (byte[])converter.ConvertTo(bitmapImage, typeof(byte[]));
+
+                images.Add(imgBytes);
+                ViewBag.Images = images;
             }
 
             return View(aulaViewModel);
