0815603b1babdfad08828cf92893d0e6f73f1a6b
Ygor Lacerda <ygor.l@financas360.com.br>
2023-10-20 12:54:12 -0300

    ajustando dpi da imagem

--- src/TCC.UI.Web/Controllers/AulasController.cs
+++ src/TCC.UI.Web/Controllers/AulasController.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 using PdfiumViewer;
 using System.Drawing;
 using System.Drawing.Imaging;
+using System.Runtime.Intrinsics.Arm;
 using TCC.Application.Interfaces;
 
 namespace TCC.UI.Web.Controllers
@@ -42,16 +44,24 @@ namespace TCC.UI.Web.Controllers
             var filePath = Path.Combine(
                 _env.ContentRootPath, "wwwroot", "assets", "pdf", aulaViewModel.ContentUrl);
 
-            using (var pdfDocument = PdfDocument.Load(filePath))
+            using (var document = PdfDocument.Load(filePath)) // C# Read PDF Document
             {
                 var images = new List<byte[]>();
+                var dpi = 300;
+                
+                for (int pageNumber = 0; pageNumber < document.PageCount; pageNumber++)
+                {
+                    SizeF sizeInPoints = document.PageSizes[pageNumber];
+                    int widthInPixels = (int)Math.Round(sizeInPoints.Width * (float)dpi / 72F);
+                    int heightInPixels = (int)Math.Round(sizeInPoints.Height * (float)dpi / 72F);
 
-                var bitmapImage = pdfDocument.Render(0, 700, 700, true);
-
-                ImageConverter converter = new ImageConverter();
-                var imgBytes = (byte[])converter.ConvertTo(bitmapImage, typeof(byte[]));
-
-                images.Add(imgBytes);
+                    using (Image image = document.Render(pageNumber, widthInPixels, heightInPixels, dpi, dpi, true))
+                    {
+                        ImageConverter converter = new ImageConverter();
+                        var imgBytes = (byte[])converter.ConvertTo(image, typeof(byte[]));
+                        images.Add(imgBytes);
+                    }
+                }
                 ViewBag.Images = images;
             }
 
