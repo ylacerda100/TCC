fa8feb3741373dc7fcd73e6d2bfae325ac397429
Ygor Lacerda <ygor.l@financas360.com.br>
2023-10-25 12:42:50 -0300

    ajuste email

--- src/TCC.UI.Web/Areas/Identity/Pages/Login/Register.cshtml.cs
+++ src/TCC.UI.Web/Areas/Identity/Pages/Login/Register.cshtml.cs
@@ -148,8 +148,8 @@ namespace TCC.UI.Web.Areas.Identity.Pages.Account
                         values: new { area = "Identity", userId = userId, code = code, returnUrl = returnUrl },
                         protocol: Request.Scheme);
 
-                    await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
-                        $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
+                    await _emailSender.SendEmailAsync(Input.Email, "Confirme seu email",
+                        $"Por favor confirme sua conta <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicando aqui</a>.");
 
                     if (_userManager.Options.SignIn.RequireConfirmedAccount)
                     {
