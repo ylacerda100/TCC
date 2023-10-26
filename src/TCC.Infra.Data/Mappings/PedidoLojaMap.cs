5e67ecf1674725ac786dcbba2f56c77f8408cb55
Ygor Lacerda <ygor.l@financas360.com.br>
2023-10-26 09:01:03 -0300

    add pedido loja map

---
+++ src/TCC.Infra.Data/Mappings/PedidoLojaMap.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TCC.Domain.Models;
+
+namespace TCC.Infra.Data.Mappings
+{
+    public class PedidoLojaMap : IEntityTypeConfiguration<PedidoLoja>
+    {
+        public void Configure(EntityTypeBuilder<PedidoLoja> builder)
+        {
+        }
+    }
+}
