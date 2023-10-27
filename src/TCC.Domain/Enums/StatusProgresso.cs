b4d5fe3098fe24922e7fb0de9b47775b60373d92
Ygor Lacerda <ygor.l@financas360.com.br>
2023-10-27 12:57:43 -0300

    adicionando nova model de progresso

---
+++ src/TCC.Domain/Enums/StatusProgresso.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TCC.Domain.Enums
+{
+    public enum StatusProgresso
+    {
+        NaoIniciado = 0,
+        EmAndamento = 1,
+        Concluido = 2
+    }
+}
---
+++ src/TCC.Domain/Models/ProgressoAula.cs
@@ -0,0 +1,26 @@
+﻿using NetDevPack.Domain;
+using System.ComponentModel.DataAnnotations.Schema;
+using TCC.Domain.Enums;
+
+namespace TCC.Domain.Models
+{
+    public class ProgressoAula : Entity, IAggregateRoot
+    {
+        [ForeignKey("Usuario")]
+        public Guid UsuarioId { get; set; }
+        public Usuario Usuario { get; set; }
+
+        [ForeignKey("Aula")]
+        public Guid AulaId { get; set; }
+
+        public Aula Aula { get; set; }
+
+        public StatusProgresso Status { get; set; }
+
+        public DateTime DataConclusao { get; set; }
+
+        public ProgressoAula()
+        {
+        }
+    }
+}
