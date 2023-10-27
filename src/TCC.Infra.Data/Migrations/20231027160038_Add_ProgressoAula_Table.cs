f52715ef99b7916c44b69b15f1140647ebd2e1f9
Ygor Lacerda <ygor.l@financas360.com.br>
2023-10-27 13:01:27 -0300

    add migration progresso

---
+++ src/TCC.Infra.Data/Migrations/20231027160038_Add_ProgressoAula_Table.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TCC.Infra.Data.Migrations
+{
+    public partial class Add_ProgressoAula_Table : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Progressos",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    UsuarioId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    AulaId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Status = table.Column<int>(type: "int", nullable: false),
+                    DataConclusao = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Progressos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Progressos_AspNetUsers_UsuarioId",
+                        column: x => x.UsuarioId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Progressos_Aulas_AulaId",
+                        column: x => x.AulaId,
+                        principalTable: "Aulas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Progressos_AulaId",
+                table: "Progressos",
+                column: "AulaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Progressos_UsuarioId",
+                table: "Progressos",
+                column: "UsuarioId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Progressos");
+        }
+    }
+}
