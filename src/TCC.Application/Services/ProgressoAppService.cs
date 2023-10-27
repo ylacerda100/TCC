01b9aaae482e6c72d131415578bb09549edc3a23
Ygor Lacerda <ygor.l@financas360.com.br>
2023-10-27 13:09:44 -0300

    add progresso app service

---
+++ src/TCC.Application/Services/ProgressoAppService.cs
@@ -0,0 +1,19 @@
+﻿using TCC.Application.Interfaces;
+using TCC.Domain.Interfaces;
+
+namespace TCC.Application.Services
+{
+    public class ProgressoAppService : IProgressoAppService
+    {
+        private readonly IProgressoAulaRepository _progressoRepo;
+        public ProgressoAppService(IProgressoAulaRepository progressoAulaRepository)
+        {
+            _progressoRepo = progressoAulaRepository;
+        }
+
+        public void Dispose()
+        {
+            GC.SuppressFinalize(this);
+        }
+    }
+}
