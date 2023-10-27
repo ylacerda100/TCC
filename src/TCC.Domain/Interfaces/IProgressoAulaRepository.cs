6800f1d3955042d1ccd2695a84a2f199e8d662be
Ygor Lacerda <ygor.l@financas360.com.br>
2023-10-27 13:04:43 -0300

    interface repo

---
+++ src/TCC.Domain/Interfaces/IProgressoAulaRepository.cs
@@ -0,0 +1,13 @@
+﻿using NetDevPack.Data;
+using TCC.Domain.Models;
+
+namespace TCC.Domain.Interfaces
+{
+    public interface IProgressoAulaRepository : IRepository<ProgressoAula>
+    {
+        Task<ProgressoAula> GetById(Guid id);
+        Task<bool> Add(ProgressoAula progresso);
+        void Update(ProgressoAula progresso);
+        Task<ProgressoAula> GetByAulaId(Guid aulaId);
+    }
+}
