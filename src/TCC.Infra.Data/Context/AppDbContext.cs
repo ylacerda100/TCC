e7f89436fe1fbf280c0966676cd72a5207237671
Ygor Lacerda <ygor.l@financas360.com.br>
2023-10-29 13:20:25 -0300

    populando itens

--- src/TCC.Infra.Data/Context/AppDbContext.cs
+++ src/TCC.Infra.Data/Context/AppDbContext.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using NetDevPack.Data;
 using NetDevPack.Messaging;
@@ -10,20 +9,20 @@ using TCC.Infra.Data.Mappings;
 namespace TCC.Infra.Data.Context;
 
 public class AppDbContext : IdentityDbContext<
-    Usuario, 
-    ApplicationRole, 
-    Guid, 
-    ApplicationUserClaim, 
-    ApplicationUserRole, 
-    ApplicationUserLogin, 
-    ApplicationRoleClaim, 
+    Usuario,
+    ApplicationRole,
+    Guid,
+    ApplicationUserClaim,
+    ApplicationUserRole,
+    ApplicationUserLogin,
+    ApplicationRoleClaim,
     ApplicationUserToken
     >, IUnitOfWork
 {
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
     {
     }
-    
+
     public DbSet<Curso> Cursos { get; set; }
     public DbSet<Aula> Aulas { get; set; }
     public DbSet<ItemLoja> Itens { get; set; }
@@ -53,10 +52,87 @@ public class AppDbContext : IdentityDbContext<
         modelBuilder.ApplyConfiguration(new UsuarioMap());
         modelBuilder.ApplyConfiguration(new ProgressoAulaMap());
         modelBuilder.ApplyConfiguration(new RespostaAlunoExercicioMap());
-
         #endregion
 
+
+        #region populateDB
+        GenerateStoreItems(modelBuilder);
+        #endregion
+
+
         base.OnModelCreating(modelBuilder);
+    }
+
+    private void GenerateStoreItems(ModelBuilder modelBuilder)
+    {
+        var itens = new List<ItemLoja>
+        {
+            new ItemLoja
+            {
+                Id = Guid.NewGuid(),
+                Nome = "Boost de XP",
+                Descricao = "Boost de XP de 75%",
+                Preco = 500,
+                ImagemUrl = "75-XP.png",
+                Duracao = TimeSpan.FromDays(3).Ticks,
+                TipoItem = Domain.Enums.TipoItemLoja.Boost,
+                Multiplicador = 0.75m
+            },
+            new ItemLoja
+            {
+                Id = Guid.NewGuid(),
+                Nome = "Boost de XP",
+                Descricao = "Boost de XP de 50%",
+                Preco = 400,
+                ImagemUrl = "50-XP.png",
+                Duracao = TimeSpan.FromDays(3).Ticks,
+                TipoItem = Domain.Enums.TipoItemLoja.Boost,
+                Multiplicador = 0.50m
+            },
+            new ItemLoja
+            {
+                Id = Guid.NewGuid(),
+                Nome = "Boost de XP",
+                Descricao = "Boost de XP de 25%",
+                Preco = 300,
+                ImagemUrl = "25-XP.png",
+                Duracao = TimeSpan.FromDays(3).Ticks,
+                TipoItem = Domain.Enums.TipoItemLoja.Boost,
+                Multiplicador = 0.25m
+            },
+            new ItemLoja
+            {
+                Id = Guid.NewGuid(),
+                Nome = "Pacote de XP",
+                Descricao = "Receba 250 em XP",
+                Preco = 450,
+                ImagemUrl = "250-XP.png",
+                TipoItem = Domain.Enums.TipoItemLoja.PacoteXp,
+                QtdXp = 250
+            },
+            new ItemLoja
+            {
+                Id = Guid.NewGuid(),
+                Nome = "Pacote de XP",
+                Descricao = "Receba 500 em XP",
+                Preco = 900,
+                ImagemUrl = "500-XP.png",
+                TipoItem = Domain.Enums.TipoItemLoja.PacoteXp,
+                QtdXp = 500
+            },
+            new ItemLoja
+            {
+                Id = Guid.NewGuid(),
+                Nome = "Pacote de XP",
+                Descricao = "Receba 1000 em XP",
+                Preco = 1800,
+                ImagemUrl = "1000-XP.png",
+                TipoItem = Domain.Enums.TipoItemLoja.PacoteXp,
+                QtdXp = 1000
+            }
+        };
+
+        modelBuilder.Entity<ItemLoja>().HasData(itens);
     }
 
     public async Task<bool> Commit()
