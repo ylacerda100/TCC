ee8a60148f4ea1d65f4acba0a68e237c07138fc9
Ygor Lacerda <ygor.l@financas360.com.br>
2023-10-30 22:50:53 -0300

    ajuste mensagens de erro

--- src/TCC.UI.Web/Areas/Identity/Pages/Account/Manage/Email.cshtml.cs
+++ src/TCC.UI.Web/Areas/Identity/Pages/Account/Manage/Email.cshtml.cs
@@ -69,7 +69,7 @@ namespace TCC.UI.Web.Areas.Identity.Pages.Account.Manage
             ///     directly from your code. This API may change or be removed in future releases.
             /// </summary>
             [Required(ErrorMessage = "O campo Novo Email é obrigatório!")]
-            [EmailAddress]
+            [EmailAddress(ErrorMessage = "O email é inválido.")]
             [Display(Name = "Novo email")]
             public string NewEmail { get; set; }
         }
